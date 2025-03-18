2cc4214d8859011be67982a81c55a41b6d17b32e
FIC <fic@info.local>
2025-03-17 21:41:29 -0300

    Projeto inicial

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// Estruturas Condicionais 
+
+// Etruturas de Repeticao 
+// Repete uma acao
+
+// Imprimir todos os numeros de 1 a 5 
+
+// enquanto - while 
+
+//  Repete uma acao ENQUANTO algo for verdade 
+
+//int numero = 1;
+
+// while ( numero < 6 )
+// //   Console.WriteLine(numero);
+// Incremento
+//numero = numero + 1;
+//  numero++;
+//
+// Faça  - Enquanto (Do - While)
+//int idade = int.Parse(Console.ReadLine());
+
+//while (idade != 17)
+//{
+// Console.WriteLine("Informe a idade: ");
+//   idade = int.Parse(Console.ReadLine()); 
+
+//int idade;
+
+//do
+//    Console.WriteLine("informe a idade: ");
+//   idade = int .Parse(Console.ReadLine()); 
+//} while (idade != 17);
+
+
+// Para - for 
+// Quero imprimir os numeros de 1 a 5
+
+// 1- cria uma variavel chamada I (indice)
+// 2- Condição para continuar 
+// 3- Incremento ao executar as chaves 
+
+for (int i = 0; i < 18; i++)
+{
+    Console.WriteLine(i);  
+}
